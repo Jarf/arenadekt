c681fae56c3d58ea1a4dc4c72235d3bdf57cd331
Martin Wadeson <martin.wadeson@ccsw.co.uk>
2024-10-29 14:28:12 +0000

    Added print statements to import script

--- scripts/cardImport.php
+++ scripts/cardImport.php
@@ -1,11 +1,14 @@
 <?php
+print 'Init...';
 require_once(dirname(__DIR__) . '/include/config.php');
 require_once(dirname(__DIR__) . '/include/autoload.php');
 
 $download = !ISDEV;
+print 'Done' . PHP_EOL;
 
 $carddata = $bulkurl = null;
 if($download){
+	print 'Downloading Bulk Data Listing...';
 	$ch = curl_init();
 	$scryfallheader = array('User-Agent: arenadekt','Accept: application/json');
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -15,21 +18,28 @@ if($download){
 	$response = @json_decode($response);
 	curl_close($ch);
 	if(!empty($response) && isset($response->data)){
+		print 'Done' . PHP_EOL;
 		foreach($response->data as &$list){
 			if(isset($list->object) && $list->object === 'bulk_data' && isset($list->type) && $list->type === 'oracle_cards' && isset($list->download_uri) && !empty($list->download_uri)){
 				$bulkurl = $list->download_uri;
+				print 'Found Bulk Data URL...';
 				break;
 			}
 		}
 
 		if(!empty($bulkurl)){
+			print 'Downloading...';
 			$ch = curl_init();
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($ch, CURLOPT_URL, $bulkurl);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $scryfallheader);
 			$carddata = curl_exec($ch);
 			$carddata = @json_decode($carddata);
+			curl_close($ch);
+			print 'Done' . PHP_EOL;
 		}
+	}else{
+		print 'Failed' . PHP_EOL;
 	}
 }else{
 	$carddata = file_get_contents(dirname(__FILE__) . '/oracle-cards-20241029090205.json');
@@ -37,6 +47,7 @@ if($download){
 }
 
 if(!empty($carddata)){
+	print 'Parsing Card Data';
 	$db = new db();
 	$insert = $bindparams = array();
 	$i = 0;
@@ -56,11 +67,15 @@ if(!empty($carddata)){
 			$insert[] = '(:name' . $i . ',:alchemy' . $i . ')';
 			$i++;
 		}
+		print '.';
 	}
+	print 'Done' . PHP_EOL;
+	print 'Inserting Data';
 	$chunksize = 500;
 	$insert = array_chunk($insert, $chunksize);
 	$bindparams = array_chunk($bindparams, $chunksize * 2, true);
 	foreach($insert as $chunkkey => $insertdata){
+		print '.';
 		$sql = 'INSERT INTO cards (name,alchemy) VALUES ' . implode(',', $insertdata) . ' ON DUPLICATE KEY UPDATE name=VALUES(name),alchemy=VALUES(alchemy)';
 		$db->query($sql);
 		foreach($bindparams[$chunkkey] as $bindkey => $bindval){
@@ -68,6 +83,9 @@ if(!empty($carddata)){
 		}
 		$db->execute();
 	}
+	print 'Done' . PHP_EOL;
+}else{
+	print 'Failed To Retrieve Card Data' . PHP_EOL;
 }
 
 ?>
