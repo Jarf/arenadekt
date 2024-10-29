6a117f49b8d46a3423ece187b90f6d9ae9d642a5
Martin Wadeson <martin.wadeson@gmail.com>
2024-10-29 15:12:29 +0000

    Use jsonreader to limit memory usage

--- include/autoload.php
+++ include/autoload.php
@@ -1,6 +1,6 @@
 <?php
 // Vendor Autoload
-// require_once(DIR_VENDOR . 'autoload.php');
+require_once(DIR_VENDOR . 'autoload.php');
 // Common Functions
 require_once(DIR_INCLUDE . 'common_functions.php');
 // Abstract Classes Autoload
--- include/config.php
+++ include/config.php
@@ -10,17 +10,12 @@ define('DB_TYPE', 'mysql');
 define('DIR_ROOT', dirname(__DIR__) . '/');
 define('DIR_VENDOR', DIR_ROOT . 'vendor/');
 define('DIR_CSS', DIR_ROOT . 'css/');
-define('DIR_JS', DIR_ROOT . 'js/');
 define('DIR_INCLUDE', DIR_ROOT . 'include/');
-define('DIR_PAGES', DIR_ROOT . 'pages/');
-define('DIR_TPL', DIR_ROOT . 'tpl/');
-define('DIR_CACHE', '/tmp/');
 define('DIR_CLASSES', DIR_ROOT . 'classes/');
 define('DIR_CLASSES_ABSTRACT', DIR_CLASSES . 'abstract/');
 
 // Site Paths
 define('SITE_CSS', '/css/');
-define('SITE_JS', '/js/');
 
 // Site Vars
 $servername = isset($_SERVER['SERVER_NAME']) ? $_SERVER['SERVER_NAME'] : 'arenadekt.jarfjam.co.uk';
